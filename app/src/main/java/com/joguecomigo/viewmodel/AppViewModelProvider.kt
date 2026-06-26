6daebe9828f1250cc1f94f95ae4a05f48a5a45d8
wvfc <154296335+wvfc@users.noreply.github.com>
2026-06-25 23:15:24 -0400

    feat: registra TicTacToeGameViewModel no AppViewModelProvider

--- app/src/main/java/com/joguecomigo/viewmodel/AppViewModelProvider.kt
+++ app/src/main/java/com/joguecomigo/viewmodel/AppViewModelProvider.kt
@@ -13,20 +13,35 @@ import com.joguecomigo.JogueComigoApp
 object AppViewModelProvider {
     val Factory = viewModelFactory {
         initializer {
-            SettingsViewModel(app().container.settingsManager, app().container.progressManager)
+            SettingsViewModel(
+                app().container.settingsManager,
+                app().container.progressManager,
+            )
         }
         initializer {
             ProgressViewModel(app().container.progressManager)
         }
         initializer {
-            SudokuGameViewModel(app().container.progressManager, app().container.settingsManager)
+            SudokuGameViewModel(
+                app().container.progressManager,
+                app().container.settingsManager,
+            )
         }
         initializer {
-            CheckersGameViewModel(app().container.progressManager, app().container.settingsManager)
+            CheckersGameViewModel(
+                app().container.progressManager,
+                app().container.settingsManager,
+            )
+        }
+        initializer {
+            TicTacToeGameViewModel(
+                app().container.progressManager,
+                app().container.settingsManager,
+            )
         }
     }
 }
 
-/** Recupera a instância de [JogueComigoApp] a partir das CreationExtras. */
-private fun CreationExtras.app(): JogueComigoApp =
+/** Extensão utilitária para recuperar o [JogueComigoApp] dentro de [CreationExtras]. */
+fun CreationExtras.app(): JogueComigoApp =
     this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as JogueComigoApp
